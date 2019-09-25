a6efc5978ff49d9bf70029afcd4e419fef31d609
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2019-09-25 09:04:40 +0200

    change order of files

--- src/Mark.elm
+++ src/Mark.elm
@@ -13,64 +13,6 @@ module Mark exposing
 import Html exposing (Html)
 
 
-partitionByTerm : Options a -> String -> String -> List a
-partitionByTerm options term content =
-    if options.minTermLength < String.length term then
-        let
-            indexes =
-                case options.searchType of
-                    SearchNormal CaseIgnore _ ->
-                        String.indexes (String.toLower term) (String.toLower content)
-
-                    SearchNormal CaseSensitive _ ->
-                        String.indexes term content
-
-                    SearchCustom getIndexes ->
-                        getIndexes term content
-        in
-        partitionByTermHelp options (List.reverse indexes) term content []
-
-    else
-        [ options.missWrapper content ]
-
-
-partitionByTermHelp : Options a -> List Int -> String -> String -> List a -> List a
-partitionByTermHelp ({ hitWrapper, missWrapper } as options) revPositions term content markers =
-    case revPositions of
-        [] ->
-            wrapAndAddToMarkers content missWrapper markers
-
-        pos :: rest ->
-            let
-                end =
-                    pos + String.length term
-
-                miss =
-                    String.dropLeft end content
-
-                hit =
-                    String.slice pos end content
-
-                newContent =
-                    String.dropRight (String.length miss + String.length hit) content
-
-                newMarkers =
-                    wrapAndAddToMarkers hit hitWrapper <|
-                        wrapAndAddToMarkers miss missWrapper markers
-            in
-            partitionByTermHelp options rest term newContent newMarkers
-
-
-wrapAndAddToMarkers : String -> (String -> a) -> List a -> List a
-wrapAndAddToMarkers item wrapper markers =
-    case item of
-        "" ->
-            markers
-
-        _ ->
-            wrapper item :: markers
-
-
 {-| -}
 type alias Options a =
     { searchType : SearchType
@@ -143,6 +85,62 @@ defaultOptions =
     , missWrapper = \miss -> Html.mark [] [ Html.text miss ]
     }
 
+partitionByTerm : Options a -> String -> String -> List a
+partitionByTerm options term content =
+    if options.minTermLength < String.length term then
+        let
+            indexes =
+                case options.searchType of
+                    SearchNormal CaseIgnore _ ->
+                        String.indexes (String.toLower term) (String.toLower content)
+
+                    SearchNormal CaseSensitive _ ->
+                        String.indexes term content
+
+                    SearchCustom getIndexes ->
+                        getIndexes term content
+        in
+        partitionByTermHelp options (List.reverse indexes) term content []
+
+    else
+        [ options.missWrapper content ]
+
+
+partitionByTermHelp : Options a -> List Int -> String -> String -> List a -> List a
+partitionByTermHelp ({ hitWrapper, missWrapper } as options) revPositions term content markers =
+    case revPositions of
+        [] ->
+            wrapAndAddToMarkers content missWrapper markers
+
+        pos :: rest ->
+            let
+                end =
+                    pos + String.length term
+
+                miss =
+                    String.dropLeft end content
+
+                hit =
+                    String.slice pos end content
+
+                newContent =
+                    String.dropRight (String.length miss + String.length hit) content
+
+                newMarkers =
+                    wrapAndAddToMarkers hit hitWrapper <|
+                        wrapAndAddToMarkers miss missWrapper markers
+            in
+            partitionByTermHelp options rest term newContent newMarkers
+
+
+wrapAndAddToMarkers : String -> (String -> a) -> List a -> List a
+wrapAndAddToMarkers item wrapper markers =
+    case item of
+        "" ->
+            markers
+
+        _ ->
+            wrapper item :: markers
 
 {-| -}
 highlightWith : Options a -> String -> String -> List a
