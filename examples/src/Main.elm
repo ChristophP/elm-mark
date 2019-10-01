9108d31f63e0b541258616b2dcdc6c15401d6383
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2019-10-02 00:32:48 +0200

    remove newlines from search string

--- examples/src/Main.elm
+++ examples/src/Main.elm
@@ -74,18 +74,20 @@ viewOption options =
 
 
 searchText =
-    """You can't, if you can't feel it, if it never
-Rises from the soul, and sways
-The heart of every single hearer,
-With deepest power, in simple ways.
-You'll sit forever, gluing things together,
-Cooking up a stew from other's scraps,
-Blowing on a miserable fire,
-Made from your heap of dying ash.
-Let apes and children praise your art,
-If their admiration's to your taste,
-But you'll never speak from heart to heart,
-Unless it rises up from your heart's space."""
+    String.join " "
+        [ "You can't, if you can't feel it, if it never"
+        , "Rises from the soul, and sways"
+        , "The heart of every single hearer,"
+        , "With deepest power, in simple ways."
+        , "You'll sit forever, gluing things together,"
+        , "Cooking up a stew from other's scraps,"
+        , "Blowing on a miserable fire,"
+        , "Made from your heap of dying ash."
+        , "Let apes and children praise your art,"
+        , "If their admiration's to your taste,"
+        , "But you'll never speak from heart to heart,"
+        , "Unless it rises up from your heart's space."
+        ]
 
 
 optionButton { clickMsg, name, selected } =
